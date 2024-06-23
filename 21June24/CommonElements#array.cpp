49800f01caee2bf1a6faf357016ee0337a9e9017
iamprashu <iamprashu.gs@gmail.com>
2024-06-23 22:49:20 +0530

    Changes to be committed: 	modified:   21June24/CommonElements#array.cpp 	modified:   21June24/FirstRepeatedElement.cpp

--- 21June24/CommonElements#array.cpp
+++ 21June24/CommonElements#array.cpp
@@ -5,72 +5,60 @@
 using namespace std;
 
 vector<int>commonElements(int A[], int B[], int C[], int n1, int n2, int n3){
-    // First i am initilizing 3 pointers to triverse 3 arrays 
+    // First, I am initializing 3 pointers to traverse 3 arrays 
+    int i = 0, j = 0, k = 0;
 
-    int i=0, j=0, k=0;
+    unordered_map<int, int> table; // Initializing map to store all elements as key that element should not repeat
+    vector<int> answer; // Vector to save elements and return them
 
-    unordered_map <int,int>table; // Initilizing map to store all elements as key that element should not repeat
-
-    vector<int>answer; //vector to save elements and return them
-
-    while(i<n1 && j<n2 && k<n3){
-
-        if(A[i]==B[j] && B[j] == C[k]){     // if all the pointers standing on same elements than i will store that pointer as a key in map to get uniqu element
-
-            int CommonElement = A[i]; //getting current element
-
-            if(table[CommonElement] == 0)
-            {
-                answer.push_back(CommonElement); //storing element if not present in map
+    while(i < n1 && j < n2 && k < n3){
+        if(A[i] == B[j] && B[j] == C[k]){ // If all the pointers standing on same elements then I will store that pointer as a key in map to get unique element
+            int CommonElement = A[i]; // Getting current element
+            if(table[CommonElement] == 0){
+                answer.push_back(CommonElement); // Storing element if not present in map
             }
-            //here after getting the element store in answer vector i dont want it to puch back again so i will increment it's value
+            // Here after getting the element store in answer vector, I don't want it to push back again so I will increment its value
             table[CommonElement] = table[CommonElement] + 1;
-
             i++;
             j++;
             k++;
-
         }
-
-        // if elements not match so i will move the pointers by checking between all arrays as it is a Increasing array
-
-        else if(A[i]<B[j]){
+        // If elements do not match, so I will move the pointers by checking between all arrays as it is an increasing array
+        else if(A[i] < B[j]){
             i++;
         }
-        else if(B[j]<C[k]){
+        else if(B[j] < C[k]){
             j++;
         }
-
         else{
             k++;
         }
-
     }
-
     return answer;
 }
-
 
 int main(){
     int A[] = {1, 5, 10, 20, 40, 80};
     int B[] = {6, 7, 20, 80, 100};
     int C[] = {3, 4, 15, 20, 30, 70, 80, 120};
 
-     int n1 = 6; int n2 = 5; int n3 = 8;
+    int n1 = 6; 
+    int n2 = 5; 
+    int n3 = 8;
 
     // int A[] = {1,2,3};
     // int B[] = {1,2,3};
     // int C[] = {1,2,3};
 
-    //  int n1 = 3; int n2 = 3; int n3 = 3;
+    // int n1 = 3; 
+    // int n2 = 3; 
+    // int n3 = 3;
 
-    vector<int> answer = commonElements(A,B,C,n1,n2,n3);
+    vector<int> answer = commonElements(A, B, C, n1, n2, n3);
 
-    for(int i=0; i<answer.size(); i++){
-        cout<<answer[i]<<endl;
+    for(int i = 0; i < answer.size(); i++){
+        cout << answer[i] << endl;
     }
-    
 
     return 0;
-
 }
--- 21June24/FirstRepeatedElement.cpp
+++ 21June24/FirstRepeatedElement.cpp
@@ -5,31 +5,30 @@
 using namespace std;
 
 void FirstRepeatedelement(int arr[], int size){
-    //In this Questions i am using map to get count of elements and than  i look into the map the first element that has count more than 1 i'll 
-    // return it.
+    // In this question, I am using a map to get the count of elements and then I look into the map.
+    // The first element that has a count more than 1, I'll return it.
 
-
-    unordered_map<int,int>table;//Initilizing unorderd map
+    unordered_map<int, int> table; // Initializing unordered map
     
-    for(int i=0; i<size; i++){//This will store count of each element that is present in array
+    for(int i = 0; i < size; i++){ // This will store count of each element that is present in array
         table[arr[i]] = table[arr[i]] + 1;
     }
 
-    for(int i=0; i<size; i++){
-        if(table[arr[i]]>1){
-            cout<<"First Repeated Element is : "<<arr[i]<<endl;
+    for(int i = 0; i < size; i++){ 
+        if(table[arr[i]] > 1){
+            cout << "First Repeated Element is: " << arr[i] << endl;
             return;
         }
     }
 
-    cout<<"Not Found";
+    cout << "Not Found";
 }
 
 int main(){
-    int arr[]{1,5,3,4,3,5,6};
+    int arr[] = {1, 5, 3, 4, 3, 5, 6};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    FirstRepeatedelement(arr,size);
+    FirstRepeatedelement(arr, size);
 
     return 0;
 }
